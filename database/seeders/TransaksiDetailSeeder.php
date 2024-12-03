e8dfe0f06631fbd3c23292638bbc8b70a0a966de
deandrasann <deandrasantosoo@gmail.com>
2024-12-03 13:07:47 +0700

    transaksi detail seeder

--- database/seeders/TransaksiDetailSeeder.php
+++ database/seeders/TransaksiDetailSeeder.php
@@ -21,27 +21,33 @@ class TransaksiDetailSeeder extends Seeder
         $transaksi = Transaksi::all();
 
         foreach ($transaksi as $t) {
-            $numberOfDetails = // gunakan faker untuk membuat angka antara 5 - 15
+            // Buat jumlah detail antara 5-15 menggunakan faker
+            $numberOfDetails = $faker->numberBetween(5, 15);
             $total_harga = 0;
 
             for ($j = 0; $j < $numberOfDetails; $j++) {
-                $hargaSatuan = $faker->numberBetween(10, 500) * 100;
-                $jumlah = $faker->numberBetween(1, 5);
-                $subtotal = $hargaSatuan * $jumlah;
-                $total_harga += $subtotal;
+                // Generate harga satuan dan jumlah produk
+                $hargaSatuan = $faker->numberBetween(10, 500) * 100; // Harga satuan kelipatan 100
+                $jumlah = $faker->numberBetween(1, 5); // Jumlah produk
+                $subtotal = $hargaSatuan * $jumlah; // Subtotal
+                $total_harga += $subtotal; // Tambahkan subtotal ke total harga
 
-                TransaksiDetail:create([
+                // Buat data transaksi detail
+                TransaksiDetail::create([
                     'id_transaksi' => $t->id,
                     'nama_produk' => $faker->productName,
                     'harga_satuan' => $hargaSatuan,
                     'jumlah' => $jumlah,
-                    'subtotal'
+                    'subtotal' => $subtotal,
                 ]);
             }
 
-            $t->total_harga = $total_harga;
-            $t->bayar = ceil($total_harga/50000) * 50000;
-            $t->kembalian = $t->bayar - $total_harga;
+            // Update total harga di tabel transaksi
+            $t->update([
+                'total_harga' => $total_harga,
+                'bayar' => ceil($total_harga / 50000) * 50000, // Pembulatan ke atas kelipatan 50.000
+                'kembalian' => ceil($total_harga / 50000) * 50000 - $total_harga,
+            ]);
         }
     }
 }
