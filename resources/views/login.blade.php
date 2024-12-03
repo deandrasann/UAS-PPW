c589565d9228f7ba734ce9174b6cb189c4ddf222
deandrasann <deandrasantosoo@gmail.com>
2024-12-03 13:45:44 +0700

    view login 2

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,6 @@
+
+@extends('layouts')
+@section('content')
 <div class="row justify-content-center mt-5">
     <div class="col-md-8">
 
@@ -37,3 +40,4 @@
         </div>
     </div>
 </div>
+@endsection
